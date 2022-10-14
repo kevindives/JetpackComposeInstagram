07da026253a95d99c623349ace54a63fcc445b2e
kevindives <kevinsagm@gmail.com>
2022-10-13 19:20:19 -0500

    Se agrega una card tipo twitter

--- twiterchallenge/src/main/java/com/magicworld/twiterchallenge/MainActivity.kt
+++ twiterchallenge/src/main/java/com/magicworld/twiterchallenge/MainActivity.kt
@@ -3,13 +3,26 @@ package com.magicworld.twiterchallenge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.magicworld.twiterchallenge.ui.theme.JetpackComposeInstagramTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +31,138 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeInstagramTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color(0xFF161022))
+                        .verticalScroll(rememberScrollState())
                 ) {
-                    Greeting("Android")
+                    MyTwitterCard()
+                    TwitDivider()
+                    MyTwitterCard()
+                    TwitDivider()
+                    MyTwitterCard()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyTwitterCard() {
+
+    var chat by rememberSaveable { mutableStateOf(false) }
+    var reTwitt by rememberSaveable { mutableStateOf(false) }
+    var like by rememberSaveable { mutableStateOf(false) }
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF161022))
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "",
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(55.dp)
+        )
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row(Modifier.fillMaxWidth()) {
+                MyTextTitle(title = "kevin", modifier = Modifier.padding(end = 8.dp))
+                MyDefaultText(title = "@KevinSagm", modifier = Modifier.padding(end = 8.dp))
+                MyDefaultText(title = "4h", modifier = Modifier.padding(end = 8.dp))
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    painterResource(id = R.drawable.ic_dots),
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+            TextBody(
+                text = "Este es un ejemplo de prograacion " +
+                        "hecho por arisdev en donde estamos replicando " +
+                        "una card con compse y esta quedando muy bien " +
+                        "espero que les guste y que sigan a aris "
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(vertical = 12.dp)
+                    .clip(RoundedCornerShape(10)),
+                contentScale = ContentScale.Crop
+            )
+            Row(Modifier.fillMaxWidth()) {
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unSelectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_chat),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    SelectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_chat_filled),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    isSelected = chat
+                ) {
+                    chat = !chat
+                }
+
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unSelectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    SelectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_rt),
+                            contentDescription = "",
+                            tint = Color(0xFF00FF27)
+                        )
+                    },
+                    isSelected = reTwitt
+                ) {
+                    reTwitt = !reTwitt
+                }
+
+                SocialIcon(
+                    modifier = Modifier.weight(1f),
+                    unSelectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_like),
+                            contentDescription = "",
+                            tint = Color(0xFF7E8B98)
+                        )
+                    },
+                    SelectedIcon = {
+                        Icon(
+                            painterResource(id = R.drawable.ic_like_filled),
+                            contentDescription = "",
+                            tint = Color(0xFFFF0000)
+                        )
+                    },
+                    isSelected = like
+                ) {
+                    like = !like
                 }
             }
         }
@@ -30,14 +170,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SocialIcon(
+    modifier: Modifier,
+    unSelectedIcon: @Composable () -> Unit,
+    SelectedIcon: @Composable () -> Unit,
+    isSelected: Boolean,
+    onItemSelected: () -> Unit
+) {
+
+    val defaultValue = 1
+
+    Row(
+        modifier = modifier.clickable { onItemSelected() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (isSelected) SelectedIcon() else unSelectedIcon()
+        Text(
+            text = if (isSelected) (defaultValue + 1).toString() else defaultValue.toString(),
+            color = Color(0xFF7E8b98),
+            fontSize = 12.sp,
+            modifier = Modifier.padding(start = 4.dp)
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeInstagramTheme {
-        Greeting("Android")
-    }
+fun MyTextTitle(title: String, modifier: Modifier = Modifier) {
+    Text(text = title, color = Color.White, fontWeight = FontWeight.ExtraBold, modifier = modifier)
 }
+
+@Composable
+fun MyDefaultText(title: String, modifier: Modifier) {
+    Text(text = title, color = Color.Gray, modifier = modifier)
+}
+
+@Composable
+fun TextBody(text: String, modifier: Modifier = Modifier) {
+    Text(text = text, color = Color.White, modifier = modifier)
+}
+
+@Composable
+fun TwitDivider() {
+    Divider(
+        Modifier
+            .padding(top = 4.dp)
+            .height(0.5.dp)
+            .fillMaxWidth(),
+        color = Color(0xFF7E8B98)
+    )
+}
+
+
